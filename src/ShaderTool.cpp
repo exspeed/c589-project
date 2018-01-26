0acfa138febfafe86861a3ea4f400f95a79e80b2
atheng <atheng@ucalgary.ca>
2018-01-26 11:50:36 -0700

    move geometry and shader to it's own class

---
+++ src/ShaderTool.cpp
@@ -0,0 +1,81 @@
+#include "../include/ShaderTool.h"
+
+// --------------------------------------------------------------------------
+// OpenGL shader support functions
+
+// reads a text file with the given name into a std::string
+std::string LoadSource(const std::string &filename)
+{
+	std::string source;
+
+	std::ifstream input(filename.c_str());
+	if (input) {
+		copy(std::istreambuf_iterator<char>(input),
+			std::istreambuf_iterator<char>(),
+			back_inserter(source));
+		input.close();
+	}
+	else {
+		std::cout << "ERROR: Could not load shader source from file "
+			<< filename << std::endl;
+	}
+
+	return source;
+}
+
+// creates and returns a shader object compiled from the given source
+GLuint CompileShader(GLenum shaderType, const std::string &source)
+{
+	// allocate shader object name
+	GLuint shaderObject = glCreateShader(shaderType);
+
+	// try compiling the source as a shader of the given type
+	const GLchar *source_ptr = source.c_str();
+	glShaderSource(shaderObject, 1, &source_ptr, 0);
+	glCompileShader(shaderObject);
+
+	// retrieve compile status
+	GLint status;
+	glGetShaderiv(shaderObject, GL_COMPILE_STATUS, &status);
+	if (status == GL_FALSE)
+	{
+		GLint length;
+		glGetShaderiv(shaderObject, GL_INFO_LOG_LENGTH, &length);
+		std::string info(length, ' ');
+		glGetShaderInfoLog(shaderObject, info.length(), &length, &info[0]);
+		std::cerr << "ERROR compiling shader:" << std::endl << std::endl;
+		std::cerr << source << std::endl;
+		std::cerr << info << std::endl;
+	}
+
+	return shaderObject;
+}
+
+// creates and returns a program object linked from vertex and fragment shaders
+GLuint LinkProgram(GLuint vertexShader, GLuint fragmentShader)
+{
+	// allocate program object name
+	GLuint programObject = glCreateProgram();
+
+	// attach provided shader objects to this program
+	if (vertexShader)   glAttachShader(programObject, vertexShader);
+	if (fragmentShader) glAttachShader(programObject, fragmentShader);
+
+	// try linking the program with given attachments
+	glLinkProgram(programObject);
+
+	// retrieve link status
+	GLint status;
+	glGetProgramiv(programObject, GL_LINK_STATUS, &status);
+	if (status == GL_FALSE)
+	{
+		GLint length;
+		glGetProgramiv(programObject, GL_INFO_LOG_LENGTH, &length);
+		std::string info(length, ' ');
+		glGetProgramInfoLog(programObject, info.length(), &length, &info[0]);
+		std::cout << "ERROR linking shader program:" << std::endl;
+		std::cout << info << std::endl;
+	}
+
+	return programObject;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -25,18 +25,14 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+#include "../include/Geometry.h"
+#include "../include/ShaderTool.h"
 // --------------------------------------------------------------------------
 // OpenGL utility and support function prototypes
 
 void QueryGLVersion();
 bool CheckGLErrors();
 
-std::string LoadSource(const std::string &filename);
-GLuint CompileShader(GLenum shaderType, const std::string &source);
-GLuint LinkProgram(GLuint vertexShader, GLuint fragmentShader);
-
-// --------------------------------------------------------------------------
-// Functions to set up OpenGL shader programs for rendering
 
 // load, compile, and link shaders, returning true if successful
 GLuint InitializeShaders()
@@ -59,23 +55,6 @@ GLuint InitializeShaders()
 	// check for OpenGL errors and return false if error occurred
 	return program;
 }
-
-// --------------------------------------------------------------------------
-// Functions to set up OpenGL buffers for storing geometry data
-
-struct Geometry
-{
-	// OpenGL names for array buffer objects, vertex array object
-	GLuint  vertexBuffer;
-	GLuint  textureBuffer;
-	GLuint  colourBuffer;
-	GLuint  vertexArray;
-	GLsizei elementCount;
-
-	// initialize object names to zero (OpenGL reserved value)
-	Geometry() : vertexBuffer(0), colourBuffer(0), vertexArray(0), elementCount(0)
-	{}
-};
 
 bool InitializeVAO(Geometry *geometry){
 
@@ -326,82 +305,6 @@ bool CheckGLErrors()
 	return error;
 }
 
-// --------------------------------------------------------------------------
-// OpenGL shader support functions
 
-// reads a text file with the given name into a std::string
-std::string LoadSource(const std::string &filename)
-{
-	std::string source;
 
-	std::ifstream input(filename.c_str());
-	if (input) {
-		copy(std::istreambuf_iterator<char>(input),
-			std::istreambuf_iterator<char>(),
-			back_inserter(source));
-		input.close();
-	}
-	else {
-		std::cout << "ERROR: Could not load shader source from file "
-			<< filename << std::endl;
-	}
 
-	return source;
-}
-
-// creates and returns a shader object compiled from the given source
-GLuint CompileShader(GLenum shaderType, const std::string &source)
-{
-	// allocate shader object name
-	GLuint shaderObject = glCreateShader(shaderType);
-
-	// try compiling the source as a shader of the given type
-	const GLchar *source_ptr = source.c_str();
-	glShaderSource(shaderObject, 1, &source_ptr, 0);
-	glCompileShader(shaderObject);
-
-	// retrieve compile status
-	GLint status;
-	glGetShaderiv(shaderObject, GL_COMPILE_STATUS, &status);
-	if (status == GL_FALSE)
-	{
-		GLint length;
-		glGetShaderiv(shaderObject, GL_INFO_LOG_LENGTH, &length);
-		std::string info(length, ' ');
-		glGetShaderInfoLog(shaderObject, info.length(), &length, &info[0]);
-		std::cout << "ERROR compiling shader:" << std::endl << std::endl;
-		std::cout << source << std::endl;
-		std::cout << info << std::endl;
-	}
-
-	return shaderObject;
-}
-
-// creates and returns a program object linked from vertex and fragment shaders
-GLuint LinkProgram(GLuint vertexShader, GLuint fragmentShader)
-{
-	// allocate program object name
-	GLuint programObject = glCreateProgram();
-
-	// attach provided shader objects to this program
-	if (vertexShader)   glAttachShader(programObject, vertexShader);
-	if (fragmentShader) glAttachShader(programObject, fragmentShader);
-
-	// try linking the program with given attachments
-	glLinkProgram(programObject);
-
-	// retrieve link status
-	GLint status;
-	glGetProgramiv(programObject, GL_LINK_STATUS, &status);
-	if (status == GL_FALSE)
-	{
-		GLint length;
-		glGetProgramiv(programObject, GL_INFO_LOG_LENGTH, &length);
-		std::string info(length, ' ');
-		glGetProgramInfoLog(programObject, info.length(), &length, &info[0]);
-		std::cout << "ERROR linking shader program:" << std::endl;
-		std::cout << info << std::endl;
-	}
-
-	return programObject;
-}
